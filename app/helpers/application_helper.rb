a080a0a7bd43a8e35512e2fe425d4ca1eba30997
Claus Skou Nielsen <kalusn@gmail.com>
2011-12-03 23:57:56 +0100

    layout changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,17 @@ module ApplicationHelper
   end
 
   def pretty_date(date)
-    date.strftime("%d. %B %Y")
+    case date.day
+    when 1, 21, 31
+      append = "st"
+    when 2, 22
+      append = "nd"
+    when 3, 23
+      append = "rd"
+    else
+      append = "th"
+    end
+
+    date.strftime("The %d#{append} of %B, %Y")
   end
 end
